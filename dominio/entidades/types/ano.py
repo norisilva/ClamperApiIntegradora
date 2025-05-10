b4bae068dcf79e4d5cb0c63b961f2ba17940de52
Norivan Silva <norivan.oliveira@gmail.com>
2025-05-10 19:29:32 -0300

    task: Criação sincronização produtos novos protheus

--- dominio/entidades/types/ano.py
+++ dominio/entidades/types/ano.py
@@ -1,4 +1,4 @@
-class Ano:
+class ano:
     def __init__(self, valor: int):
         if valor < 1900 or valor > 2500:
             raise ValueError("O ano deve estar entre 1900 e 2500.")
--- dominio/entidades/types/fabricante.py
+++ dominio/entidades/types/fabricante.py
@@ -1,7 +1,7 @@
-class Nome:
+class fabricante:
     def __init__(self, valor: str):
         if not valor or len(valor.strip()) < 2:
-            raise ValueError("O nome deve ter pelo menos 2 caracteres.")
+            raise ValueError("O fabricante deve ter pelo menos 2 caracteres.")
         self._valor = valor.strip()
 
     @property
